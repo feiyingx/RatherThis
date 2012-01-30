416a78cbdd70a142f265b4af7aa48c37a137b961
Feiyingx <feiyingx@gmail.com>
2012-01-29 19:55:42 -0800

    meta keywords for google

--- RatherThis/obj/Debug/Package/PackageTmp/Scripts/global.js
+++ RatherThis/obj/Debug/Package/PackageTmp/Scripts/global.js
@@ -14,8 +14,6 @@ function loadComments(containerId, loaderId, questionId, listSize) {
     $.get("/Question/CommentList?qid=" + questionId + "&listSize=" + listSize, function (result) {
         var container = $("#" + containerId);
         if (container) $(container).html(result);
-        var loader = $("#" + loaderId);
-        if (loader) $(loader).hide();
     });
 }
 
